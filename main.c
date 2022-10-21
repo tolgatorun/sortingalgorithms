7e1627c7ce48bb064607b316680c599506590816
Tolga Torun <dtolgatorun@gmail.com>
2022-10-21 12:51:14 +0300

    quick and merge sort

--- main.c
+++ main.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-//int int_exponential(int x, int y);
 void insertion_sort(int arr[], int n);
 void selection_sort(int arr[], int n);
 void bubble_sort(int arr[],int n);
 void heapify(int arr[], int i);
 void build_heap(int arr[], int n);
 void heap_sort(int arr[],int n);
+void quick_sort(int arr[], int left, int right);
+void merge_sort(int arr[], int left, int right);
 float execution_time(void (*f)(int[], int),int,int[]);
+float execution_time_3variable(void (*f)(int[], int, int),int,int[], int);
 int heap_size;
+int max_size = 1000000;
 int main(void) {
   int decision = 0;
   while(decision == 0) {
@@ -25,6 +28,7 @@ int main(void) {
     case 1:
       {
         int array_size;
+        clock_t s, e;
         heap_size = array_size;
         printf("%s", "Please enter an integer for array size: ");
         scanf("%d", &array_size);
@@ -37,26 +41,39 @@ int main(void) {
           printf("%d\n", arr[i]);
         }*/
         double exectime;
+        
         exectime = execution_time(insertion_sort,array_size, arr);
         exectime = exectime / CLOCKS_PER_SEC;
         printf("%s%f%s", "Insertion Sort:",exectime, "\n");
+
         exectime = execution_time(selection_sort,array_size, arr);
         exectime = exectime / CLOCKS_PER_SEC;
         printf("%s%f%s", "Selection Sort:",exectime, "\n");
+
         exectime = execution_time(bubble_sort,array_size, arr);
         exectime = exectime / CLOCKS_PER_SEC;
         printf("%s%f%s", "Bubble Sort",exectime, "\n");
+
         exectime = execution_time(heap_sort,array_size, arr);
         exectime = exectime / CLOCKS_PER_SEC;
         printf("%s%f%s", "Heap Sort",exectime, "\n");
+
+        exectime = execution_time_3variable(quick_sort, 0, arr,array_size-1);
+        exectime = exectime / CLOCKS_PER_SEC;
+        printf("%s%f%s", "Quick Sort: ",exectime, "\n");
+        
+        exectime = execution_time_3variable(merge_sort, 0, arr,array_size-1);
+        exectime = exectime / CLOCKS_PER_SEC;
+        printf("%s%f%s", "Merge Sort: ",exectime, "\n");
         break;
       }
     case 2:
       {
         int array_size = 10;
+        clock_t s, e;
         heap_size = array_size;
         double exectime;
-        for(int i=1; i<9; i++) {
+        for(int i=1; i<7; i++) {
           int* arr;
           //array_size = int_exponential(10, i);
           arr = (int *)malloc(array_size * sizeof(int));
@@ -67,21 +84,32 @@ int main(void) {
          /*
           for(int k = 0; k<array_size; k++) {
             if ((k + 100) > array_size) { printf("%d%s%d%s", k, "element is: ", arr[k], "\n"); }
-          }
-          */
-          printf("%ld%s", array_size * sizeof(int), "\n");
+          }*/
+          printf("%s%d%s", "Size of array: ", array_size, "\n");
+          
           exectime = execution_time(insertion_sort,array_size, arr);
           exectime = exectime / CLOCKS_PER_SEC;
           printf("%s%f%s", "Insertion Sort:",exectime, "\n");
+          
           exectime = execution_time(selection_sort,array_size, arr);
           exectime = exectime / CLOCKS_PER_SEC;
           printf("%s%f%s", "Selection Sort:",exectime, "\n");
+          
           exectime = execution_time(bubble_sort,array_size, arr);
           exectime = exectime / CLOCKS_PER_SEC;
-          printf("%s%f%s", "Bubble Sort",exectime, "\n");
+          printf("%s%f%s", "Bubble Sort: ",exectime, "\n");
+          
           exectime = execution_time(heap_sort,array_size, arr);
           exectime = exectime / CLOCKS_PER_SEC;
-          printf("%s%f%s", "Heap Sort",exectime, "\n");
+          printf("%s%f%s", "Heap Sort: ",exectime, "\n");
+
+          exectime = execution_time_3variable(quick_sort, 0, arr,array_size-1);
+          exectime = exectime / CLOCKS_PER_SEC;
+          printf("%s%f%s", "Quick Sort: ",exectime, "\n");
+          
+          exectime = execution_time_3variable(merge_sort, 0, arr,array_size-1);
+          exectime = exectime / CLOCKS_PER_SEC;
+          printf("%s%f%s", "Merge sort: ",exectime, "\n");
           free(arr);
           array_size = array_size * 10;
         }
@@ -93,6 +121,13 @@ int main(void) {
 float execution_time(void (*f)(int*, int),int n,int arr[]) {
   clock_t start = clock();
   (*f)(arr, n);
+  clock_t end = clock();
+  return end - start;
+}
+
+float execution_time_3variable(void (*f)(int[], int, int),int left,int arr[], int right) {
+  clock_t start = clock();
+  (*f)(arr, left, right);
   clock_t end = clock();
   return end - start;
 }
@@ -142,6 +177,7 @@ void bubble_sort(int arr[],int n) {
 void heapify(int arr[], int i) {
   int left(int i);
   int right(int i);
+
   int left_child, right_child, max, temp;
   left_child = left(i);
   right_child = right(i);
@@ -189,12 +225,79 @@ void heap_sort(int arr[], int n) {
     heapify(arr, 0);
   }
 }
-/*
-int int_exponential(int x, int y) {
-  int start = 1;
-  for(int i =0; i<y; i++) {
-  start = start * x;
+
+void quick_sort(int arr[], int left, int right) {
+  int k, j, q,temp;
+  k = left;
+  j = right;
+  
+  q = arr[(left+right)/2];
+
+  do {
+    while((arr[k] < q) && (k < right)) {
+      k++;
+    }
+    while((arr[j] > q) && (j > left)) {
+      j--;
+    }
+    if (k <= j) {
+      temp = arr[k];
+      arr[k] = arr[j];
+      arr[j] = temp;
+      k++;
+      j--;
+    }
+  }while(k <= j);
+  
+  if (left < j) {
+    quick_sort(arr, left, j);
   }
-  return start;
+  if (k < right) {
+    quick_sort(arr, k, right);
+  }
 }
-*/
+
+void merge_sort(int arr[], int left, int right) {
+  void merge(int arr[], int left, int k, int right);
+
+  int k;
+  if (left < right) {
+    k = (left + right)/2;
+    merge_sort(arr, left, k);
+    merge_sort(arr, k+1, right);
+
+    merge(arr, left, k, right);
+    }
+  }
+
+void merge(int arr[], int left, int k, int right) {
+  int i, j, l = 0;
+  int* temp_arr;
+  temp_arr = (int *)malloc(max_size * sizeof(int));
+
+  for (i=left, j = k+1; (i <=k) && (j <= right);) {
+    if (arr[i] < arr[j]) {
+      temp_arr[l] = arr[i];
+      i++;
+      l++;
+    }
+    else {
+      temp_arr[l] = arr[j];
+      j++;
+      l++;
+    }
+  }
+  while(i <=k) {
+    temp_arr[l] = arr[i];
+    i++;
+    l++;
+  }
+  while (j <= right) {
+    temp_arr[l] = arr[j];
+    j++;
+    l++;
+  }
+  for (i = left, l = 0; i<= right; i++, l++) {
+    arr[i]= temp_arr[l];
+  }
+}
